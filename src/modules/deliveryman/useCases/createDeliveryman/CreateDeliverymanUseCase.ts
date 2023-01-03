957eb017dcebd6acd11aaeb675e215c7473e0b7e
Douglas Santos <douglas_motasts@outlook.com>
2023-01-03 13:14:33 -0300

    feature: corrigindo busca de pedidos do cliente e adicionando validacao para criação de novos users

--- src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -12,6 +12,7 @@ export class CreateDeliverymanUseCase {
         const clientExist = await prisma.deliveryman.findFirst({ 
             where: {
                 username: {
+                    equals: username,
                     mode: 'insensitive'
                 }
             }
